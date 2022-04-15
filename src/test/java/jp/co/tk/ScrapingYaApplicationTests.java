88e315abce9ea7c31691080f9e0d916e2c9dd8f6
TAKAHIRO-03 <1701010402qb@cyber-u.ac.jp>
2022-04-15 23:08:22 +0900

    second commit

---
+++ src/test/java/jp/co/tk/ScrapingYaApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.tk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapingYaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
