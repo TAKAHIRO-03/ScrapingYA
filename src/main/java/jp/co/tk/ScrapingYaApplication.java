e90d6c1d4be1c1599688335b59740f505bf9dffd
1701010402qb@cyber-u.ac.jp <fabhqg0318>
2022-04-15 23:08:22 +0900

    second commit

---
+++ src/main/java/jp/co/tk/ScrapingYaApplication.java
@@ -0,0 +1,13 @@
+package jp.co.tk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapingYaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrapingYaApplication.class, args);
+    }
+
+}
